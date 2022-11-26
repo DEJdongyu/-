78d1c7dc49ded66567b1fa4ebd45f88bb15836cb
DEJDongYu <552081051@qq.com>
2022-11-26 20:35:50 +0800

    DongYu

--- 核酸检测管理系统/server.py
+++ 核酸检测管理系统/server.py
@@ -1,11 +1,17 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
+@app.route("/",methods=["GET", "POST"])
+def home():
     return render_template("home.html")
+
+
+@app.route("/templates/user1.html",methods=["GET", "POST"])
+def user1():
+    return render_template("user1.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
